17d40592ffcf5986d9d2b0ecdf49fd48efb0eb73
N-SooHyun <nsh4842@gmail.com>
2025-02-28 01:40:21 +0900

    태그별 데이터별 파싱 완

--- xml_parser.cpp
+++ xml_parser.cpp
@@ -57,31 +57,77 @@ SAX는 필요한 데이터만 뽑아 쓰는 느낌으로 진행하여
 
 namespace nDynamic {
 	class DynamicStr {
-		int current_size;
-
 	public:
-		char* p_d_char;
-		int capacity;
-		DynamicStr(int size) : capacity(size), current_size(0){
-			p_d_char = new char[capacity];
+		char* p_d_str;
+		int current_size_str;
+		int capacity_str;
+
+		char* p_d_data;		//속성일때는 데이터
+		int current_size_data;
+		int capacity_data;
+		DynamicStr(int size) : capacity_str(size), current_size_str(0), p_d_data(nullptr){
+			p_d_str = new char[capacity_str];
 		}
-		~DynamicStr() { delete p_d_char; }
+		~DynamicStr() { 
+			if (p_d_str != nullptr)
+				delete p_d_str; 
+		}
+
+		void OperStr(int pos, char c) {
+			if (pos >= capacity_str) {
+				SizeUpStr();
+			}
+			p_d_str[pos] = c;
+			current_size_str++;
+		}
+		void OperData(int pos, char c) {
+			if (pos >= capacity_str) {
+				SizeUpStr();
+			}
+			p_d_data[pos] = c;
+			current_size_data++;
+		}
+
 		
 		void SizeUpStr() {
-			int old_capacity = capacity;
-			capacity *= 2;
-			char* temp = new char[capacity];
+			int old_capacity = capacity_str;
+			capacity_str *= 2;
+			char* temp = new char[capacity_str];
 			for (int i = 0; i < old_capacity; i++) {
-				temp[i] = p_d_char[i];
+				temp[i] = p_d_str[i];
 			}
-			delete p_d_char;
-			p_d_char = temp;
+			delete p_d_str;
+			p_d_str = temp;
 		}
 
 		void EmptyStr(int init_capacity_size) {
-			delete p_d_char;
-			p_d_char = new char[init_capacity_size];
+			delete p_d_str;
+			p_d_str = new char[init_capacity_size];
+			current_size_str = 0;
+			capacity_str = init_capacity_size;
 		}
+
+		void FitSizeStr() {
+			char* temp = new char[current_size_str+1];
+			for (int i = 0; i < current_size_str; i++) {
+				temp[i] = p_d_str[i];
+			}
+			temp[current_size_str] = '\0';
+			delete p_d_str;
+			p_d_str = temp;
+		}
+
+		void FitSizeData() {
+			char* temp = new char[current_size_data + 1];
+			for (int i = 0; i < current_size_str; i++) {
+				temp[i] = p_d_data[i];
+			}
+			temp[current_size_str] = '\0';
+			delete p_d_data;
+			p_d_data = temp;
+		}
+
+		
 	};
 }
 
@@ -91,14 +137,14 @@ namespace nFile {
 		FILE* pFile = NULL;
 		char* pXml_Content;
 		int capacity;
-		int current_size;
+		int current_size_str;
 
 		ReadFile(char* path) {
 			//fopen_s(&pFile, path, "r");
 			fopen_s(&pFile, path, "r");
 			capacity = 1024;
 			pXml_Content = new char[capacity];
-			current_size = 0;
+			current_size_str = 0;
 			CopyFile();
 		}
 		~ReadFile() {
@@ -108,27 +154,27 @@ namespace nFile {
 		void CopyFile() {
 			char c;
 			char* temp;
-			for (current_size = 0; ; current_size++) {
+			for (current_size_str = 0; ; current_size_str++) {
 				c = fgetc(pFile);
 				if (c == EOF) break;
-				else if (current_size == capacity - 1) {
+				else if (current_size_str == capacity - 1) {
 					capacity *= 2;
 					temp = pXml_Content;
 					pXml_Content = new char[capacity];
-					for (int i = 0; i < current_size; i++) {
+					for (int i = 0; i < current_size_str; i++) {
 						pXml_Content[i] = temp[i];
 					}
 					delete temp;
 				}
-				pXml_Content[current_size] = c;
+				pXml_Content[current_size_str] = c;
 			}
-			pXml_Content[current_size] = '\0';
-			temp = new char[current_size+1];
-			for (int i = 0; i<current_size; i++) {
+			pXml_Content[current_size_str] = '\0';
+			temp = new char[current_size_str+1];
+			for (int i = 0; i<current_size_str; i++) {
 				temp[i] = pXml_Content[i];
 			}
-			temp[current_size] = '\0';
-			capacity = current_size;
+			temp[current_size_str] = '\0';
+			capacity = current_size_str;
 			delete pXml_Content;
 			pXml_Content = temp;
 			fclose(pFile);
@@ -137,17 +183,36 @@ namespace nFile {
 }
 
 namespace nXml_Parser {
+	class XmlObj {
+	public:
+		char *obj_tag_name;		//동적 배열 이름
+		//추후 속성 멤버 리소스 필요
+		XmlObj* child_obj;		//동적 배열 자식 태그들
+		int child_obj_size;		//동적 배열 자식 태그들의 개수
+		void* data;				//혹은 태그가 없고 데이터만 있는 경우
+
+		XmlObj(char *_obj_tag_name, int name_size) : obj_tag_name(new char[name_size+1]) {
+			for (int i = 0; i < name_size; i++) {
+				obj_tag_name[i] = _obj_tag_name[i];		//이름 복사
+			}
+			obj_tag_name[name_size] = '\0';
+		}
+		XmlObj() {}
+
+		
+	};
+
 	using namespace nDynamic;
 	void XmlFileResCk(nFile::ReadFile* xml_file) {
 		printf("file에 대한 리소스\n");
 		printf("받아온 xml의 용량(capacity) : %d\n", xml_file->capacity);
-		printf("크기(current_size) : %d\n", xml_file->current_size);
+		printf("크기(current_size_str) : %d\n", xml_file->current_size_str);
 	}
 
 	void XmlWordCkToObj(nFile::ReadFile* xml_file) {
 		char c;
 		int obj_cnt = 0;
-		for (int i = 0; i < xml_file->current_size; i++) {
+		for (int i = 0; i < xml_file->current_size_str; i++) {
 			c = xml_file->pXml_Content[i];
 			if (c == '<' && (c= xml_file->pXml_Content[++i]) == '/') {
 				obj_cnt++;
@@ -161,37 +226,64 @@ namespace nXml_Parser {
 		char c; 
 		int data_cnt = 0;
 		int data_size = 1024;
-		DynamicStr data(data_size);
-		for (int i = 0; (i < xml_file->current_size); i++) {
-			for (;; i++) {
-				if ((c = xml_file->pXml_Content[i]) != ' ') break;
-			}
+		int tag_size = 1024;
+		int attr_cnt = 1024;				//속성개수
+		DynamicStr data(data_size);			//데이터
+		DynamicStr tag_name(tag_size);		//태그이름
+		
+		XmlObj* tag_root = new XmlObj();
+		XmlObj* tag;
+
+		bool tr_end_tag = false;
+		bool tr_attr_input = false;
+
+		for (int i = 0; (i < xml_file->current_size_str); i++) {
 			c = xml_file->pXml_Content[i];
-			if (c == '<') {		//태그일때
-				for (;; i++) {
-					c = xml_file->pXml_Content[i];
-					//속성 추가 하는 거 추후 만들 예정
-					if (c == '>') {
-						//객체 생성
-						c = xml_file->pXml_Content[++i];
-						break;
-					}
+			for (;; i++) {
+				c = xml_file->pXml_Content[i];
+				if (c == '\n' || c == ' ') {
+					continue;
+				}
+				else {
+					break;
 				}
 			}
-			if (c != '\n' && c!=' ' && c != '\0') {	//데이터일때
-				char ck_word;
+			if (c == '<') {		//태그일때
+				tr_end_tag = false;
+				c = xml_file->pXml_Content[++i];
 				for (int j = 0; ; i++, j++) {
-					if (j >= data.capacity - 2)
-						data.SizeUpStr();
 					c = xml_file->pXml_Content[i];
-					data.p_d_char[j] = c;
-					if ((ck_word = xml_file->pXml_Content[i + 1]) == '<' && (ck_word = xml_file->pXml_Content[i + 2]) == '/') {
-						data.p_d_char[j + 1] = '\0';
+					if (c == '>') break;
+					if ((c == '/') && (xml_file->pXml_Content[i-1] == '<')) tr_end_tag = true;
+					else if (c == ' ') {}//attr_input = true;	//속성입력
+					if (tr_end_tag != true) {		//태그 이름 넣어주기
+						tag_name.OperStr(j, c);
+					}
+				}
+				if (tr_end_tag != true) {			//태그 종료 아닐때 태그 생성
+					if (tr_attr_input == true) {	//속성값 있다면
+
+					}
+					tag_name.FitSizeStr();
+					tag = new XmlObj(tag_name.p_d_str, tag_name.current_size_str);
+					printf("Tag : %s\n", tag_name.p_d_str);
+					tag_name.EmptyStr(tag_size);
+				}
+			}
+			//데이터일때
+			if (c != '>' && tr_end_tag == false) {
+				char ck;
+				for (int j = 0; ; i++,j++) {
+					c = xml_file->pXml_Content[i];
+					data.OperStr(j, c);
+					if (((ck = xml_file->pXml_Content[i + 1]) == '<') && ((ck = xml_file->pXml_Content[i + 2]) == '/')) {
+						i += 2;
+						tr_end_tag = true;
 						break;
 					}
 				}
-				data_cnt++;
-				printf("%d번째 Data : %s\n", data_cnt, data.p_d_char);
+				data.FitSizeStr();
+				//printf("data : %s\n", data.p_d_str);
 				data.EmptyStr(data_size);
 			}
 		}
@@ -206,6 +298,9 @@ namespace nXml_Parser {
 		XmlFileResCk(&test);
 		XmlWordCkToObj(&test);
 		XmlObjParserTree(&test);
+
+		
+
 	}
 }
 
